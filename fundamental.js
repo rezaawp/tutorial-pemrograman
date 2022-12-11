b205284cf0b584c3c659585db3a6be79c5c7f614
rezaawp <rezaaawp@gmail.com>
2022-12-10 22:55:43 -0800

    initial commit

---
+++ fundamental.js
@@ -0,0 +1,11 @@
+const fundamental = {
+  pembukaan:
+    "Hampir semua bahasa pemrograman mempunyai konsep yang tidak jauh berbeda dalam setiap penulisan kode nya",
+  disclamer:
+    "Disini saya memberika tutorial mengguanakan bahasa pemrograman javascript",
+  javascript:
+    "Javascript adalah bahasa pemrograman yang dianamis typing. Maksud dari dinamis typing disini adalah,, kita tidak perlu menentukan tipe data apa ketika kita ingin membuat variable dan function. Kalian jangan memusingkan apa itu variable dan apa itu function.. Nanti akan dijelakan apa itu variable dan apa itu function. Javascript merupakan salah satu bahasa pemrograman yang fleksibel, dengan javascript anda bisa mmebuat aplikasi yang mulit  ",
+  syntax:
+    "Syntax adalah peraturan penulisan dalam setiap bahasa pemrograman. Di setiap bahasa pemrograman memiliki cara penulisan yang berbeda beda. Tetapi memiliki konsep yang sama. Jadi belajar dasar dasar atau basic pemrograman adalah hal yang penting untuk menjadi programmer",
+  comments: "Adalah baris kode yang digunakan untuk",
+};
